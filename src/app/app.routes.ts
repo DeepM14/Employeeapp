74c5f360d0a2678c202c523d1179460564c302c2
abc <abc@example.com>
2025-01-22 17:49:07 +0530

    Adding Code to all the Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Page/login/login.component';
+import { DashboardComponent } from './Page/dashboard/dashboard.component';
+import { LayoutComponent } from './Page/layout/layout.component';
+import { EmployeeComponent } from './Page/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'employee',
+                component:EmployeeComponent
+            }
+        ]
+    }
+];
